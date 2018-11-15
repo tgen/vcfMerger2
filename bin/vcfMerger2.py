6184e6f53c624aa98f80855e2ae8d711b7c9d4a5
clegendre_pc <clegendre@tgen.org>
2018-11-15 16:12:35 -0700

    fix bug with threshold-AR

--- bin/vcfMerger2.py
+++ bin/vcfMerger2.py
@@ -407,7 +407,7 @@ def main(args, cmdline):
 	TH_AR = 0.90
 	if args['threshold_AR']:
 		TH_AR = args['threshold_AR']
-		if isinstance(TH_AR, (int, long, float, complex)):
+		if isinstance(TH_AR, (int, float, complex)):
 			raise "Threshold-AR must be a float or integer value between 0 and 1 (range ]0,1]). Check your inputs."
 		log.info("given threshold for AR:" + str(TH_AR))
 
