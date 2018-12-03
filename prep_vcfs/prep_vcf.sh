34c3b62716e141419cc2da974d6a23fc058f69b4
clegendre <clegendre@tgen.org>
2018-12-03 12:09:19 -0700

    debug

--- prep_vcfs/prep_vcf.sh
+++ prep_vcfs/prep_vcf.sh
@@ -140,12 +140,9 @@ function init_some_vars(){
 function getOptions(){ 
 
 
-# options may be followed by one colon to indicate they have a required argument
-    options=`getopt -o hd:b:g:o:t: -l help,dir-work:,ref-genome:,tumor-sname:,normal-sname:,vcf-indels:,vcf-snvs:,
-    vcf:,toolname:,prepped-vcf-outfilename:,bam:,contigs-file:,print-default-toolnames,do-not-normalize,threshold-AR: -- "$@" `
-    echo "${options}"
-
-    if [[ ! options=`getopt -o hd:b:g:o: -l help,dir-work:,ref-genome:,tumor-sname:,normal-sname:,vcf-indels:,vcf-snvs:,vcf:,toolname:,prepped-vcf-outfilename:,bam:,contigs-file:,print-default-toolnames,do-not-normalize,threshold-AR: -- "$@" ` ]]
+    # options may be followed by one colon to indicate they have a required argument
+    if [[ ! options=`getopt -o hd:b:g:o:t: -l help,dir-work:,ref-genome:,tumor-sname:,normal-sname:,vcf-indels:,
+    vcf-snvs:,vcf:,toolname:,prepped-vcf-outfilename:,bam:,contigs-file:,print-default-toolnames,do-not-normalize,threshold-AR: -- "$@" ` ]]
 	then
 	# if something went wrong, getopt will put out an error message for us
 		echo "ERROR in Arguments" ; usage
