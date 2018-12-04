7b32b02b3f42f098271cb67d941268554c6f81db
clegendre_pc <clegendre@tgen.org>
2018-12-04 14:44:23 -0700

    added make_bed_for_venn

--- prep_vcfs/prep_vcf.sh
+++ prep_vcfs/prep_vcf.sh
@@ -139,7 +139,7 @@ function init_some_vars(){
 
 function getOptions(){
 # options may be followed by one colon to indicate they have a required argument
-if ! options=`getopt -o hd:b:g:o: -l help,dir-work:,ref-genome:,tumor-sname:,normal-sname:,vcf-indels:,vcf-snvs:,vcf:,toolname:,prepped-vcf-outfilename:,bam:,contigs-file:,print-default-toolnames,do-not-normalize -- "$@" `
+if ! options=`getopt -o hd:b:g:o: -l help,dir-work:,ref-genome:,tumor-sname:,normal-sname:,vcf-indels:,vcf-snvs:,vcf:,toolname:,prepped-vcf-outfilename:,bam:,contigs-file:,print-default-toolnames,do-not-normalize,make-bed-for-venn -- "$@" `
 	then
 	# something went wrong, getopt will put out an error message for us
 		echo "ERROR in Arguments" ; usage
