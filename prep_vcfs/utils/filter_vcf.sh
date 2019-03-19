84b0042dce77f209b93a9a6002702cbac867decd
clegendre_pc <clegendre@tgen.org>
2019-03-19 13:00:10 -0700

    test checkout branch devel

--- prep_vcfs/utils/filter_vcf.sh
+++ prep_vcfs/utils/filter_vcf.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 SNPSIFT_JAR_PATH=$1
-STRING_FOR_SNPSIFT_FILTERING="$2"  ## double quote mandatory here
-VCF=$3
+CODE_EXT_FILT=$2  # can be either "filt" or "pass"
+STRING_FOR_SNPSIFT_FILTERING="$3"  ## double quote mandatory here
+VCF=$4
 
 if [[ ${VCF##*.} == "vcf" ]] ; then
-    VCF_OUT=$(basename ${VCF} ".vcf").filt.vcf
+    VCF_OUT=$(basename ${VCF} ".vcf").${CODE_EXT_FILT}.vcf
 elif [[ ${VCF##*.} == "gz" ]] ; then
-    VCF_OUT=$(basename ${VCF} ".vcf.gz").filt.vcf
+    VCF_OUT=$(basename ${VCF} ".vcf.gz").${CODE_EXT_FILT}.vcf
 else
     echo -e "ERROR: Unexpected file extension. File Extension is neither .vcf or .vcf.gz; \
     Please Comply to the expected file extension of the VCF file; Aborting."
