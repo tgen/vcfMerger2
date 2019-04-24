24bfe1cb197d645c38346eec8acc592f56fa50a8
clegendre_pc <clegendre@tgen.org>
2019-04-23 18:11:47 -0700

    fixing tool precedence after running prep_vcf. Refixing Reference check line; Adding DIROUT for temp file

--- bin/vcfMerger2.py
+++ bin/vcfMerger2.py
@@ -773,7 +773,7 @@ def check_inputs(lvcfs, ltoolnames, ltpo=None, lacronyms=None, lprepped_vcf_outf
 			dn)
 		log.error(msg);
 		raise (msg)
-	elif dn == '':
+	elif dn == '' or dn is None:
 		log.info("final vcf will be written in current directory.")
 	log.info("filename for the uncompressed merged output vcf will be: " + merged_vcf_outfilename)
 	log.info("filename for the bgzip-compressed merged output vcf will be: " + merged_vcf_outfilename + ".gz")
