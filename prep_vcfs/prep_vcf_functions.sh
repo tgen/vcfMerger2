931916d91eadf8c0436461a7ce6b95a3b1e7192d
clegendre_pc <clegendre@tgen.org>
2019-04-25 11:19:38 -0700

    added check sample name for NORMAL and TUMOR in either column 10 or 11

--- prep_vcfs/prep_vcf_functions.sh
+++ prep_vcfs/prep_vcf_functions.sh
@@ -191,7 +191,7 @@ function check_and_update_sample_names(){
 	COL10_VALUE=`grep -m1 -E "^#CHROM" ${VCF} | cut -f10`
 	COL11_VALUE=`grep -m1 -E "^#CHROM" ${VCF} | cut -f11`
 
-	if [[ "${COL10_VALUE}" == "NORMAL" && "${COL11_VALUE}" == "TUMOR"  ]] ;
+	if [[ ( "${COL10_VALUE}" == "NORMAL" && "${COL11_VALUE}" == "TUMOR" ) ||  "${COL10_VALUE}" == "TUMOR" && "${COL11_VALUE}" == "NORMAL" ]] ;
 	then
 		echo -e "\tsample name in column 10 is  NORMAL and column 11 is named TUMOR" 1>&2
 		echo -e "\twe are updating the sample names appropriately here with the ones given by the user" 1>&2
