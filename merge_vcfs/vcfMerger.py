2ebc013eabb8837b73ee980c5a2f7efefae66188
clegendre_pc <clegendre@tgen.org>
2019-04-29 19:26:30 -0700

    test saveOverlaps

--- merge_vcfs/vcfMerger.py
+++ merge_vcfs/vcfMerger.py
@@ -400,16 +400,16 @@ def main(args, cmdline):
 		if lbeds == "":
 			exit("ERROR: list of bed files for making Venn/Upset plots MUST be provided while using --do-venn option")
 		## make venn for snvs_and_indels altogether
-		dvm.make_venn(ltoolnames, lbeds, variantType="Snvs_and_Indels", saveOverlapsBool=True, upsetBool=False, dirout=dirout+".sindels")
+		dvm.make_venn(ltoolnames, lbeds, variantType="Snvs_and_Indels", saveOverlapsBool=True, upsetBool=False, dirout=dirout)
 		## make Venn using only the SNVs
 		lbeds_snvs = [re.sub(r'\.intervene\.bed$', '.intervene.snvs.bed', file) for file in lbeds]
 		log.info(str(lbeds_snvs))
 		if all([path.isfile(f) for f in lbeds_snvs]):
-			dvm.make_venn(ltoolnames, lbeds_snvs, variantType="Snvs", saveOverlapsBool=False, upsetBool=False,  dirout=dirout+".snvs")
+			dvm.make_venn(ltoolnames, lbeds_snvs, variantType="Snvs", saveOverlapsBool=False, upsetBool=False,  dirout=dirout)
 		## make Venn using only the Indels
 		lbeds_indels = [re.sub(r'\.intervene\.bed$', '.intervene.indels.bed', file) for file in lbeds]
 		if all([path.isfile(f) for f in lbeds_indels]):
-			dvm.make_venn(ltoolnames, lbeds_indels, variantType="Indels", saveOverlapsBool=False, upsetBool=False,  dirout=dirout+".indels")
+			dvm.make_venn(ltoolnames, lbeds_indels, variantType="Indels", saveOverlapsBool=False, upsetBool=False,  dirout=dirout)
 		log.info("###########  END SECTION MAKING VENN ###############")
 
 
