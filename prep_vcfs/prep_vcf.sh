74190dc2d58dd4c40770b07585a6f729dcd4f900
clegendre_pc <clegendre@tgen.org>
2019-06-06 14:17:18 -0700

    testing why prep_vcf.sh does not work as an independent script

--- prep_vcfs/prep_vcf.sh
+++ prep_vcfs/prep_vcf.sh
@@ -28,7 +28,7 @@
 ### Minor Contributors:
 
 ## set -ue ## DO not uncomment this line; it you do so, the script does not work anymore. Why? unknown
-set -ue -o pipefail
+set -euo pipefail
 
 ## trap to capture the exit value from a function and from within a function
 trap "exit 1" TERM
