f5950831a67e2569e1f5687916754a58ec0f1e8d
clegendre_pc <clegendre@tgen.org>
2019-06-07 12:24:35 -0700

    adding license lines to file

--- prep_vcfs_germline/prep_vcf_germline.sh
+++ prep_vcfs_germline/prep_vcf_germline.sh
@@ -47,7 +47,7 @@ VALID_TOOLNAMES="deepvariant, haplotypecaller,  freebayes,  octopus,  strelka2, 
 
 type python >/dev/null 2>&1 || { echo >&2 "Require \"python\" executable but it's not in the PATH.  Aborting."; exit
 1; } || python -V
-for NEEDED_MODULE in cyvcf2 intervene collections argparse getopt gzip json logging Pillow natsort shutil subprocess warnings
+for NEEDED_MODULE in cyvcf2 intervene collections argparse getopt gzip json logging PIL natsort shutil subprocess warnings
 do
 python -c "import ${NEEDED_MODULE}"
 if [[ $? -ne 0 ]] ; then echo -e "ERROR: module << ${NEEDED_MODULE} NOT FOUND >> with this python version `python -V`; \nPlease Install module cyvcf2 or change the python version accrodingly; ABORTING\n" ; fi
