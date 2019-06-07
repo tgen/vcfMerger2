ad581381a633dcfa750ff8b03ef4b815fbaa4821
ChristopheLegendre <clegendre@tgen.org>
2019-06-07 12:16:21 -0700

    update prep_vcf files to make usaable as a standalone script as well

--- prep_vcfs/prep_vcf.sh
+++ prep_vcfs/prep_vcf.sh
@@ -51,6 +51,13 @@ OCT, SLK, VDJ|VDT, respectively [case Insensitive]"  ## if tools are later added
 
 type python >/dev/null 2>&1 || { echo >&2 "Require \"python\" executable but it's not in the PATH.  Aborting."; exit
 1; } || python -V
+
+for NEEDED_MODULE in cyvcf2 intervene collections argparse getopt gzip json logging Pillow natsort shutil subprocess warnings
+do
+python -c "import ${NEEDED_MODULE}"
+if [[ $? -ne 0 ]] ; then echo -e "ERROR: module << ${NEEDED_MODULE} NOT FOUND >> with this python version `python -V`; \nPlease Install module cyvcf2 or change the python version accrodingly; ABORTING\n" ; fi
+done
+
 #python_main_version_number=`python3 -V 2>&1 | sed 's/Python //g' | cut -d"." -f1 `
 #if [[ ! "${python_main_version_number}" == "3" ]] ; then echo -e "ERROR: Python 3 or up Expected in PATH; Aborting "
 # ; exit 1 ; fi
@@ -59,10 +66,11 @@ vt --version
 type bcftools >/dev/null 2>&1 || { echo >&2 "Require \"bcftools\" executable but it's not in the PATH.  Aborting."; exit 1; }
 if [[ $( echo "`bcftools --version-only  2>&1 | sed 's/+.*//g'` <  1.7 " | bc -l ) -eq 1  ]] ; then echo -e "ERROR: bcftools 1.7 or up Expected in PATH; Aborting " ; exit 1 ; fi
 
+# sourcing functions
 source ${DIR_PATH_TO_SCRIPTS}/prep_vcf_functions.sh
 ## init variables
 init_some_vars
-## get options
+## get options given by user
 getOptions $@
 ## recap inputs
 recap_input
