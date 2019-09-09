177685d5370f2f3737dff71e66f501c4a9225c1f
Chris <ChristopheLegendre@users.noreply.github.com>
2019-09-09 10:33:52 -0700

    added options "id-separator" to avoid error phASER

--- prep_vcfs_somatic/strelka2/strelka2.phasing_consecutives_variants_as_blocs.sh
+++ prep_vcfs_somatic/strelka2/strelka2.phasing_consecutives_variants_as_blocs.sh
@@ -126,7 +126,8 @@ VCF_OUT_PHASER=phased_${VCF_IN}
 TEMP_DIR=$(dirname ${VCF_IN} )/temp_phASER
 mkdir -p ${TEMP_DIR}
 
-mycmd="${PHASER_EXE} --bam ${TBAM} --sample ${SNAME_T}  --vcf ${VCF_IN}  --o ${VCF_OUT_PHASER}  --mapq 20 --baseq 20 --paired_end 1 --gw_phase_vcf 0  --remove_dups 1 --write_vcf 1  --gw_af_field AR --gw_phase_vcf 2 --temp_dir ${TEMP_DIR} --max_block_size 10 --threads ${CPUS}"  ## HARDCODED value for options here ; NOTE: 10 CPUS is a good trade-off between I/O and CPUS; Too amany cpus leads to too many samtools view which increases I/O and waiting time
+## HARDCODED value for options here ; NOTE: 8 CPUS is a good trade-off between I/O and CPUS; Too amany cpus leads to too many samtools view which increases I/O and waiting time
+mycmd="${PHASER_EXE} --bam ${TBAM} --sample ${SNAME_T}  --vcf ${VCF_IN}  --o ${VCF_OUT_PHASER}  --mapq 20 --baseq 20 --paired_end 1 --gw_phase_vcf 0  --remove_dups 1 --write_vcf 1  --gw_af_field AR --gw_phase_vcf 2 --temp_dir ${TEMP_DIR} --max_block_size 10 --threads ${CPUS} --id_separator ';' --unique_ids 1 "  
 echo -e "${mycmd}"
 eval ${mycmd} | tee log_for_phASER_$(basename ${VCF_OUT_PHASER} ).log
 check_ev $? "phASER using ${PHASER_EXE} with python-2.7.13"
