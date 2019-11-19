5f696a51f0cce7fd09f43449d359194abb2053fa
clegendre_pc <clegendre@tgen.org>
2019-11-19 15:51:46 -0700

    TBSHOOT OCTOPUS AR AD

--- prep_vcfs_somatic/octopus/octopus.somatic.addFieldsForVcfMerger.py
+++ prep_vcfs_somatic/octopus/octopus.somatic.addFieldsForVcfMerger.py
@@ -34,9 +34,6 @@
 ## We are not removing
 
 
-
-
-
 from sys import exit
 from sys import argv
 from os import path
@@ -50,22 +47,25 @@ from math import isnan
 global AR_threshold_for_GT
 AR_threshold_for_GT = 0.90  ## value HARDCODED but dynamically modify it with --threshold_AR option
 
+
 class Genotype(object):
-    '''
-    # genotypes = [Genotype(li) for li in variant.genotypes ]
+	'''
+	# genotypes = [Genotype(li) for li in variant.genotypes ]
 	# print genotypes
 	# which shows: [./., ./., ./., 1/1, 0/0, 0/0, 0/0]
-    '''
-    __slots__ = ('alleles', 'phased')
+	'''
+	__slots__ = ('alleles', 'phased')
 
-    def __init__(self, li):
-        self.alleles = li[:-1]
-        self.phased = li[-1]
+	def __init__(self, li):
+		self.alleles = li[:-1]
+		self.phased = li[-1]
 
-    def __str__(self):
-        sep = "/|"[int(self.phased)]
-        return sep.join("0123456."[a] for a in self.alleles)
-    __repr__ = __str__
+	def __str__(self):
+		sep = "/|"[int(self.phased)]
+		return sep.join("0123456."[a] for a in self.alleles)
+
+	__repr__ = __str__
+
 
 class GenotypeInv(object):
 
@@ -89,23 +89,23 @@ class GenotypeInv(object):
 			exit(1)
 
 	def get_gt_numpy_compatible(self):
-		self.GT = [] ## we need to reinit the GT list here otherwise shared by all instances. Weird because we reinitiated it already in the _init_ ; I am probably missing knowledge in some python features behaviour for classes.
+		self.GT = []  ## we need to reinit the GT list here otherwise shared by all instances. Weird because we reinitiated it already in the _init_ ; I am probably missing knowledge in some python features behaviour for classes.
 		if self.phased:
-			if self.allele1 != "." :
-				self.GT.append((2*int(self.allele1)) + 3)
+			if self.allele1 != ".":
+				self.GT.append((2 * int(self.allele1)) + 3)
 			else:
 				self.GT.append(1)
 			if self.allele2 != ".":
-				self.GT.append((2*int(self.allele2)) + 3)
+				self.GT.append((2 * int(self.allele2)) + 3)
 			else:
 				self.GT.append(1)
 		else:
 			if self.allele1 != ".":
-				self.GT.append((2*int(self.allele1)) + 2)
+				self.GT.append((2 * int(self.allele1)) + 2)
 			else:
 				self.GT.append(0)
 			if self.allele2 != ".":
-				self.GT.append((2*int(self.allele2)) + 2)
+				self.GT.append((2 * int(self.allele2)) + 2)
 			else:
 				self.GT.append(0)
 		return self.GT
@@ -114,7 +114,6 @@ class GenotypeInv(object):
 def usage(scriptname, opts):
 	print("\nUSAGE: \npython3 " + scriptname + '  --help')
 	print("python3 " + scriptname + " -i octopus.somatic.snvs.pass.vcf  --tumor_column 11 --normal_column 10  -o updated_vcf.vcf\n")
-
 
 	print("")
 	print("options available:")
@@ -126,14 +125,13 @@ def usage(scriptname, opts):
 	      )
 	print("")
 
-	print("#"*40 + "\nWARNING WARNING\n" + "#"*40 )
+	print("#" * 40 + "\nWARNING WARNING\n" + "#" * 40)
 	print("1) This script is to be used only with somatic snvs vcf having NORMAL sample in "
-	                 "column 10 and TUMOR sample in column 11; if not like this, update your vcf file to these "
-	                 "specs;\n2) and the vcf has to be decomposed as well.\n")
+	      "column 10 and TUMOR sample in column 11; if not like this, update your vcf file to these "
+	      "specs;\n2) and the vcf has to be decomposed as well.\n")
 
 
 def parseArgs(scriptname, argv):
-
 	new_vcf_name = None
 	column_tumor, column_normal = None, None
 
@@ -141,13 +139,12 @@ def parseArgs(scriptname, argv):
 	FORMAT_LOGGING = '%(levelname)s %(asctime)-15s %(module)s %(lineno)d\t %(message)s'
 	log.basicConfig(format=FORMAT_LOGGING, level=log.DEBUG)
 
-
 	try:
-		opts, args = getopt.getopt(argv,"hi:t:o:",[ "vcf=", \
-		                                            "tumor_column=", "normal_column=", \
-		                                            "debug", "outfilename=", \
-		                                            "threshold_AR=", \
-		                                            "help"] )
+		opts, args = getopt.getopt(argv, "hi:t:o:", ["vcf=", \
+		                                             "tumor_column=", "normal_column=", \
+		                                             "debug", "outfilename=", \
+		                                             "threshold_AR=", \
+		                                             "help"])
 		log.info(opts)
 
 	except getopt.GetoptError:
@@ -168,14 +165,14 @@ def parseArgs(scriptname, argv):
 			column_tumor = int(arg)
 		elif opt in ("", "--normal_column"):
 			column_normal = int(arg)
-		elif opt in ("-o","--outfilename"):
+		elif opt in ("-o", "--outfilename"):
 			new_vcf_name = arg.strip()
 		elif opt in ("-i", "--vcf"):
 			fvcf = arg
 			if not path.exists(fvcf):
-				exit("ERROR: FNF --> " +fvcf)
+				exit("ERROR: FNF --> " + fvcf)
 		else:
-			exit("Unknown Option --> " + opt )
+			exit("Unknown Option --> " + opt)
 
 	if column_tumor is None or column_normal is None:
 		usage(scriptname, opts)
@@ -185,8 +182,8 @@ def parseArgs(scriptname, argv):
 	if column_normal == column_tumor:
 		exit("ERROR: number for the columns Tumor and Normal MUST be different")
 
+	return (fvcf, column_tumor, column_normal, new_vcf_name)
 
-	return(fvcf, column_tumor, column_normal, new_vcf_name)
 
 def update_header(vcf):
 	'''
@@ -198,9 +195,9 @@ def update_header(vcf):
 	## if Adding Fields to INFO field
 	vcf.add_info_to_header(
 		{'ID': 'OGT', 'Description': ''.join([
-			                                      'Original Octopus GT fields for each sample before reassigning the GT value based on AR threshold (GT_0/1 < AR_',
-			                                      str(AR_threshold_for_GT),
-			                                      ' and GT_1/1 >= AR_', str(AR_threshold_for_GT), ' )']),
+			'Original Octopus GT fields for each sample before reassigning the GT value based on AR threshold (GT_0/1 < AR_',
+			str(AR_threshold_for_GT),
+			' and GT_1/1 >= AR_', str(AR_threshold_for_GT), ' )']),
 		 'Type': 'String', 'Number': '.'})
 
 	## Adding AR and new AD to FORMAT field
@@ -215,10 +212,12 @@ def update_header(vcf):
 
 	return vcf
 
+
 def is_obj_nan(obj):
 	if isnan(obj):
 		return True
 	return False
+
 
 def get_GT_value_from_AR(AR_value, GT_value):
 	'''
@@ -253,8 +252,6 @@ def get_GT_value_from_AR(AR_value, GT_value):
 
 		'''
 	log.debug("AR =" + str(AR_value) + " ---  AR_threshold = " + str(AR_threshold_for_GT))
-
-
 
 	try:
 		if AR_value < AR_threshold_for_GT:
@@ -301,7 +298,8 @@ def get_GT_value_from_GT_value(GT_value):
 	except TypeError:
 		log.error("ERROR: GT value not of right type ")
 	except Exception as e:
-		log.error("ERROR: Unknown Error ; Check with the Author :-( ; "+str(e))
+		log.error("ERROR: Unknown Error ; Check with the Author :-( ; " + str(e))
+
 
 def process_GTs(tot_number_samples, v, col_tumor, col_normal):
 	'''
@@ -318,16 +316,16 @@ def process_GTs(tot_number_samples, v, col_tumor, col_normal):
 		msg = "Expected 2 Samples in VCF found {}. We are suppose to treat the vcf as a SOMATIC vcf and expect two samples;  Aborting.".format(tot_number_samples)
 		raise Exception(msg)
 	## capturing original GTs and adding them to INFO field
-	v.INFO["OGT"] = ','.join([ str(Genotype(li)) for li in v.genotypes ])
+	v.INFO["OGT"] = ','.join([str(Genotype(li)) for li in v.genotypes])
 	## ReAssiging GT with value based on AR thresholds comparison to CONSTANT threshold value
-	GTs = [[0], [0]]  ; # need to init  list as we used index later for the list to replace values
-	GTOs = [ str(Genotype(li)) for li in v.genotypes ]
+	GTs = [[0], [0]];  # need to init  list as we used index later for the list to replace values
+	GTOs = [str(Genotype(li)) for li in v.genotypes]
 	ARs = v.format('AR')
 	idxN = 0 if col_normal == 10 else 1
 	idxT = 1 if col_tumor == 11 else 0
 	## we need to keep the order of the information based on the index; so the list GTs MUST be ordered;
-	GTs[idxN] = get_GT_value_from_GT_value(GTOs[idxN]) ## we do not modify the GT field for the Normal sample
-	GTs[idxT] = get_GT_value_from_AR(ARs[idxT], GTOs[idxT]) ## we do modify the GT field for the Tumor Sample based on defined threshold
+	GTs[idxN] = get_GT_value_from_GT_value(GTOs[idxN])  ## we do not modify the GT field for the Normal sample
+	GTs[idxT] = get_GT_value_from_AR(ARs[idxT], GTOs[idxT])  ## we do modify the GT field for the Tumor Sample based on defined threshold
 	v.set_format('GT', np.array(GTs))
 	log.debug("v after reassigning GT: " + str(v))
 	return v
@@ -337,11 +335,11 @@ def check_if_PS_in_FORMAT_field(vcf_cyobj, input_vcf_path, new_vcf_name, list_of
 	v1 = next(iter(vcf_cyobj))
 	log.info("Checking PS flag presence in FORMAT ...")
 	# Minimum_Expected_Fields_in_FORMAT_not_MANAGE_by_the_CODE
-	ExpectedFlags="GT:DP"
+	ExpectedFlags = "GT:DP"
 	for FIELD in list_of_fields_to_check:
 		if not FIELD in v1.FORMAT:
 			log.warning(
-				FIELD+" tag is ABSENT from the FORMAT field of OCTOPUS\nPlease Check you have run Octopus with appropriate options (i.e, with random forest option and no other filtering option; random forest filtering add ALL the adequate fields to FORMAT columns )")
+				FIELD + " tag is ABSENT from the FORMAT field of OCTOPUS\nPlease Check you have run Octopus with appropriate options (i.e, with random forest option and no other filtering option; random forest filtering add ALL the adequate fields to FORMAT columns )")
 			if ':'.join(v1.FORMAT) == ExpectedFlags:
 				log.info("FORMAT field is equivalent to {}, but we request to have these flags at least: {}; And we try to manage the rest, such as AD and AR; Would be better if you could run random_Forest_Filtering; ".format(v1.FORMAT, ExpectedFlags))
 				log.warning(
@@ -351,17 +349,20 @@ def check_if_PS_in_FORMAT_field(vcf_cyobj, input_vcf_path, new_vcf_name, list_of
 				exit()
 			else:
 
-				log.error(FIELD+" flag NOT found in FORMAT field; Aborting VCF preparation.")
-				exit(FIELD+" flag Absent")
+				log.error(FIELD + " flag NOT found in FORMAT field; Aborting VCF preparation.")
+				exit(FIELD + " flag Absent")
 		else:
-			log.info(FIELD+" flag Found")
+			log.info(FIELD + " flag Found")
+
 
 def if_dot_assign_negative_value(obj):
 	if obj != ".":
 		return obj
 	else:
 		return 0
-		# return -2
+
+
+# return -2
 
 
 def add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples):
@@ -373,58 +374,71 @@ def add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples):
 
 	idxT = 0 if int(column_tumor) == 10 else 1
 	idxN = 1 if int(column_normal) == 11 else 0
-	log.debug("___".join(str(x) for x in [ idxT, idxN ]) )
+	log.debug("___".join(str(x) for x in [idxT, idxN]))
 
 	if 'AD' in v.FORMAT:
 		log.debug(str(v))
 		log.debug("AD____TUMOR is " + str(v.format('AD')[idxT]))
-		log.debug("AD____TUMOR is "+str(v.format('AD')[idxT][0]))
+		log.debug("AD____TUMOR is " + str(v.format('AD')[idxT][0]))
+		log.debug("AD____NORMAL is " + str(v.format('AD')[idxN]))
+		log.debug("AD____NORMAL is " + str(v.format('AD')[idxN][0]))
 		## capturing Original AD and ADP
 		AD_tumor = v.format('AD')[idxT]
 		ADP_tumor = v.format('ADP')[idxT]
 		AD_normal = v.format('AD')[idxN]
 		ADP_normal = v.format('ADP')[idxN]
 
+		log.debug("ADP____TUMOR is " + str(v.format('ADP')[idxT]))
+		log.debug("ADP____NORMAL is " + str(v.format('ADP')[idxN]))
+
+		DP_tumor = v.format('DP')[idxT][0]  # if ',' in v.format('DP')[idxT] else v.format('DP')[idxT]		## returns numpy.str_
+		DP_normal = v.format('DP')[idxN][0]  # if ',' in v.format('DP')[idxN][0] else v.format('DP')[idxN]		## returns numpy.str_
+		# DP_tumor = v.format('DP')[idxT]
+		# DP_normal = v.format('DP')[idxN]
+		log.debug(str(DP_normal) + " -----  " + str(DP_tumor))
 
 		# AD_tumor = -1
 		# ADP_tumor = 600
-		#AD_normal = -1
-		#ADP_normal = 9999
+		# AD_normal = -1
+		# ADP_normal = 9999
 
 		log.debug(str(v))
 		log.debug("ADP --->  " + str(ADP_normal) + " -----  " + str(ADP_tumor))
+
 		## Re-Allocationg ADs to ADOs, new tag for Original Octopus AD flags and values
 		AD_tumor = int(if_dot_assign_negative_value(AD_tumor))
 		AD_normal = int(if_dot_assign_negative_value(AD_normal))
 		ADP_tumor = int(if_dot_assign_negative_value(ADP_tumor))
 		ADP_normal = int(if_dot_assign_negative_value(ADP_normal))
 		ADOs = [AD_normal, AD_tumor] if idxT == 1 else [AD_tumor, AD_normal]
-		log.debug("ADOs --->>>  " + str(ADOs) + " <<<<<<<<-----  ") ## ADO stands for AD Old ones
+		log.debug("ADOs --->>>  " + str(ADOs) + " <<<<<<<<-----  ")  ## ADO stands for AD Old values
 		v.set_format('ADO', np.array(ADOs))
 
-		## Calculate AR (allele ratio using AD and ADP)
+		## Calculate AR (allele ratio using AD and ADP) for TUMOR SAMPLE
 		try:
-			log.debug("before calcul AR, AD = ---->>    " + str(AD_normal) + " -----  " + str(AD_tumor))
-			log.debug("before calcul AR, ADP = ---->>    " + str(ADP_normal) + " -----  " + str(ADP_tumor))
+			log.debug("before calcul AR, AD = ---->>  AD_N, AD_T  " + str(AD_normal) + " -----  " + str(AD_tumor))
+			log.debug("before calcul AR, ADP = ---->> ADP_N, ADP_T   " + str(ADP_normal) + " -----  " + str(ADP_tumor))
 
-			if AD_tumor != "." and AD_tumor >=0:
-				AR_tumor = round(float(AD_tumor/ADP_tumor), 4)
+			if AD_tumor != "." and AD_tumor >= 0:
+				AR_tumor = round(float(AD_tumor / ADP_tumor), 4)
 				## Reformmating AD to expected VCF specs for that Reserved AD field, using the original AD and ADP values
-				AD_tumor = [ADP_tumor - AD_tumor, ADP_tumor]
+				AD_tumor = [ADP_tumor - AD_tumor, AD_tumor]
 			else:
-				AR_tumor = int(-2)
-				AD_tumor = [0, ADP_tumor]
+				AR_tumor = int(0)
+				AD_tumor = [ADP_tumor - AD_tumor, AD_tumor]
 		except ZeroDivisionError:
 			log.debug("division by zero!")
 			AD_tumor = [0, 0]
 			AR_tumor = float(0.00)
+		## Calculate AR (allele ratio using AD and ADP) for NORMAL SAMPLE
 		try:
-			if AD_normal != "." and AD_normal >=0:
+			if AD_normal != "." and AD_normal >= 0:
 				AR_normal = round(float(AD_normal / ADP_normal), 4)
 				AD_normal = [ADP_normal - AD_normal, 0]
 			else:
-				AR_normal = int(-2)
-				log.debug("AD in the else of the try --> " + str(ADP_normal))
+				AR_normal = int(0)
+				log.debug("AD in the else of the try --> where AD is either a dot or equals to zero ; AD ==" + str(AD_normal))
+				log.debug("ADP in the else of the try --> where AD is either a dot or equals to zero ; ADP == " + str(ADP_normal))
 				AD_normal = [ADP_normal, 0]
 
 		except ZeroDivisionError:
@@ -434,15 +448,11 @@ def add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples):
 
 		log.debug("AR --->> " + str(AR_normal) + " -----  " + str(AR_tumor))
 		log.debug("AD = ---->>    " + str(AD_normal) + " -----  " + str(AD_tumor))
-		log.debug("DP tumor is  : "+str(v.format('DP')[idxT][0]))
+		log.debug("DP tumor is  : " + str(v.format('DP')[idxT][0]))
 		log.debug("DP normal is  : " + str(v.format('DP')[idxN][0]))
-		DP_tumor = v.format('DP')[idxT][0] #if ',' in v.format('DP')[idxT] else v.format('DP')[idxT]		## returns numpy.str_
-		DP_normal = v.format('DP')[idxN][0] #if ',' in v.format('DP')[idxN][0] else v.format('DP')[idxN]		## returns numpy.str_
-		#DP_tumor = v.format('DP')[idxT]
-		#DP_normal = v.format('DP')[idxN]
-		log.debug(str(DP_normal) + " -----  " + str(DP_tumor))
-		if is_obj_nan(round(float(AR_tumor),4)): AR_tumor = 0.00
-		if is_obj_nan(round(float(AR_normal),4)): AR_normal = 0.00
+
+		if is_obj_nan(round(float(AR_tumor), 4)): AR_tumor = 0.00
+		if is_obj_nan(round(float(AR_normal), 4)): AR_normal = 0.00
 		if is_obj_nan(int(DP_tumor)): DP_tumor = 0.00
 		if is_obj_nan(int(DP_normal)): DP_normal = 0.00
 
@@ -450,10 +460,15 @@ def add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples):
 			ARs = [AR_tumor, AR_normal]
 			ADs = [AD_tumor, AD_normal]
 		else:
+			log.debug("idxT == " + str(idxT))
 			ARs = [AR_normal, AR_tumor]
 			ADs = [AD_normal, AD_tumor]
 
-	else:
+		log.debug("ARs --->> (N,T) " + str(ARs))
+		log.debug("ADs --->> (N,T)" + str(ADs))
+
+
+	else:  ## the else is entered only if there is no 'AD' flag in the octopus vcf
 		# Because Octopus does not provide enough information to calculate AD, we assign default if AD or ADP or any other information needed to get AD is not present in vcf
 		# values of 0,0 ## can be discussed and modify if users think differently
 		dummy_value = int(-2)  ## set dummy value for the AD when AD is absent or not capturable from octopus' vcf.
@@ -463,10 +478,10 @@ def add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples):
 
 		if 'MAP_VAF' in v.FORMAT:
 			AR_tumor = v.format('MAP_VAF')[idxT]
-			AR_normal = v.format('MAP_VAF')[idxN] if not isnan(v.format('MAP_VAF')[idxN]) else  [ float(0.00) ]
+			AR_normal = v.format('MAP_VAF')[idxN] if not isnan(v.format('MAP_VAF')[idxN]) else [float(0.00)]
 		else:
-			AR_tumor = [ dummy_value ]
-			AR_normal = [ dummy_value ]
+			AR_tumor = [dummy_value]
+			AR_normal = [dummy_value]
 
 		DP_tumor = v.format('DP')[idxT][0]
 		DP_normal = v.format('DP')[idxN][0]
@@ -479,31 +494,31 @@ def add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples):
 	log.debug("ADs  is  : " + str(ADs))
 	log.debug("ARs  is  : " + str(ARs))
 
-	log.debug("\t".join([ str(x) for x  in [ idxT, idxN , AR_tumor, AR_normal, DP_tumor, DP_normal ] ] ))
+	log.debug("\t".join([str(x) for x in [idxT, idxN, AR_tumor, AR_normal, DP_tumor, DP_normal]]))
 	v.set_format('AR', np.array(ARs))
 	v.set_format('AD', np.array(ADs))
 
 	return process_GTs(tot_number_samples, v, column_tumor, column_normal)
 
-#@#########
-#@ MAIN  ##
-#@#########
+
+# @#########
+# @ MAIN  ##
+# @#########
 if __name__ == "__main__":
 
-	vcf_path, column_tumor, column_normal, new_vcf_name = parseArgs(argv[0], argv[1:])  ; ## tth means tuple of thresholds
+	vcf_path, column_tumor, column_normal, new_vcf_name = parseArgs(argv[0], argv[1:]);  ## tth means tuple of thresholds
 	vcf = VCF(vcf_path)
 
-	log.info(' '.join([ "Constant AR threshold is: ", str(AR_threshold_for_GT) ]) )
+	log.info(' '.join(["Constant AR threshold is: ", str(AR_threshold_for_GT)]))
 
 	if new_vcf_name is None:
 		new_vcf = '.'.join([str(vcf_path), "uts.vcf"])
 	else:
 		new_vcf = new_vcf_name
 
-
 	## test if no variants in vcf:
 	try:
-		next(iter(vcf)) ## we just try to check if we have no variant at all in VCF
+		next(iter(vcf))  ## we just try to check if we have no variant at all in VCF
 	except StopIteration as si:
 		## we bring the header upto specs and write down the output file
 		log.warning("No Variants found in VCF; Creating Final Empty VCF now ..." + str(si))
@@ -515,7 +530,7 @@ if __name__ == "__main__":
 	## checking if PS flag is still present in the VCF genotype fields
 	check_if_PS_in_FORMAT_field(vcf, vcf_path, new_vcf_name, ["PS"])
 
-	vcf = VCF(vcf_path) ## as we have already consumed twice the generator; we do not want to lose any variant
+	vcf = VCF(vcf_path)  ## as we have already consumed twice the generator; we do not want to lose any variant
 	vcf = update_header(vcf)
 
 	# create a new vcf Writer using the input vcf as a template.
@@ -526,7 +541,7 @@ if __name__ == "__main__":
 		exit("ERROR: Number of Sample greater than 2; Expected 2 samples only TUMOR and NORMAL")
 
 	log.info("looping over records ...")
-	for v in vcf: ## v for variant which represents one "variant record"
+	for v in vcf:  ## v for variant which represents one "variant record"
 		v = add_new_flags(v, column_tumor, column_normal, filter, tot_number_samples)
 		if v is not None:
 			w.write_record(v)
