6125632af35cf573729bb8fdc08567ed6bd207a2
clegendre_pc <clegendre@tgen.org>
2019-11-18 18:25:07 -0700

    Found Why Issue with OCtopus Trying to Fix it

--- prep_vcfs_somatic/prep_vcf_somatic_functions.sh
+++ prep_vcfs_somatic/prep_vcf_somatic_functions.sh
@@ -568,9 +568,9 @@ function process_octopus_vcf(){
 	then
 	    echo -e "In Function process_octopus_vcf ... "  1>&2
         VCF=$( check_and_update_sample_names ${VCF} )
-        VCF=$( rename_fields_in_vcf_header_octopus_specific ${VCF} )
         VCF=$( look_for_block_substitution_in_octopus ${VCF}) ## why do we put 'look for blocs' before decompose? b/c we only use the first allele for collapsing block
         VCF=$( decompose ${VCF} )
+        VCF=$( rename_fields_in_vcf_header_octopus_specific ${VCF} )
         VCF=$( make_vcf_upto_specs_for_VcfMerger ${VCF} )
         VCF=$( normalize_vcf ${VCF})
         final_msg ${VCF}
