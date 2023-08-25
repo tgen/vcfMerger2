e200f2d5f1ce760816f81dd0143b10fa922c448f
clegendre <clegendre@tgen.org>
2023-08-25 11:11:04 -0700

    added redirection to stderr for echo statements to avoidconflict with return values

--- prep_vcfs_germline/dragen_snv/dragen_snv_prep.sh
+++ prep_vcfs_germline/dragen_snv/dragen_snv_prep.sh
@@ -10,9 +10,9 @@ function check_ev(){
     ENDCOLOR="\e[0m"
     if [ ${EV} -ne 0 ] ;
     then
-        echo -e "${RED}ERROR with: ${MSG}${ENDCOLOR}" ; echo "ERROR__ABORTING ! " ; exit 1 ;
+        echo -e "${RED}ERROR with: ${MSG}${ENDCOLOR}" ; echo "ERROR__ABORTING ! "  1>&2 ; exit 1 ;
     else
-        echo -e "${MSG}: ${GREEN}OK! ${ENDCOLOR}" ;
+        echo -e "${MSG}: ${GREEN}OK! ${ENDCOLOR}" 1>&2 ;
     fi
 }
 
@@ -23,8 +23,8 @@ function check_file(){
     ALL_OK=1
     for F in $*
     do
-        if [[ ! -e $F ]] ; then echo -e "${RED}ERROR: FNF ${ENDCOLOR} for << ${F} >> " ; ALL_OK=0; fi
-        echo -e "File ${GREEN}FOUND${ENDCOLOR}:  ${F}"
+        if [[ ! -e $F ]] ; then echo -e "${RED}ERROR: FNF ${ENDCOLOR} for << ${F} >> "  1>&2 ; ALL_OK=0; fi
+        echo -e "File ${GREEN}FOUND${ENDCOLOR}:  ${F}"  1>&2
     done
     if [[ ${ALL_OK} -eq 0 ]] ; then echo -e "${RED}File(s) NOT FOUND; Aborting${ENDCOLOR}; Check your inputs" ; exit 1 ; fi
 }
@@ -37,7 +37,7 @@ function usage(){
 
 VCF_SNV=$1
 
-check_file "${VCF_SNV}"
+check_file "${VCF_SNV}"  1>&2
 
 ENOA=1
 if [[ $# -ne ${ENOA} ]] ; then echo -e "Expected ${ENOA} args found $# ; Aborting;" ; usage ; exit 1 ; fi
@@ -48,6 +48,6 @@ VCF_PREPPED_OUTFILENAME=${VCF_SNV%.*}.prep.vcf
 bcftools view --threads 2 -O v "${VCF_SNV}"  | \
 bcftools +fill-tags - -- -t 'FORMAT/AR=AF' | \
 bcftools view -O v -o "${VCF_PREPPED_OUTFILENAME}"
-check_ev $? "bcftools snv prep"
+check_ev $? "bcftools snv prep"  1>&2
 
 echo ${VCF_PREPPED_OUTFILENAME}
--- prep_vcfs_germline/dragen_sv/dragen_sv_prep.sh
+++ prep_vcfs_germline/dragen_sv/dragen_sv_prep.sh
@@ -10,9 +10,9 @@ function check_ev(){
     ENDCOLOR="\e[0m"
     if [ ${EV} -ne 0 ] ;
     then
-        echo -e "${RED}ERROR with: ${MSG}${ENDCOLOR}" ; echo "ERROR__ABORTING ! " ; exit 1 ;
+        echo -e "${RED}ERROR with: ${MSG}${ENDCOLOR}" ; echo "ERROR__ABORTING ! "  1>&2 ; exit 1 ;
     else
-        echo -e "${MSG}: ${GREEN}OK! ${ENDCOLOR}" ;
+        echo -e "${MSG}: ${GREEN}OK! ${ENDCOLOR}"  1>&2;
     fi
 }
 
@@ -23,10 +23,10 @@ function check_file(){
     ALL_OK=1
     for F in $*
     do
-        if [[ ! -e $F ]] ; then echo -e "${RED}ERROR: FNF ${ENDCOLOR} for << ${F} >> " ; ALL_OK=0; fi
-        echo -e "File ${GREEN}FOUND${ENDCOLOR}:  ${F}"
+        if [[ ! -e $F ]] ; then echo -e "${RED}ERROR: FNF ${ENDCOLOR} for << ${F} >> "  1>&2 ; ALL_OK=0; fi
+        echo -e "File ${GREEN}FOUND${ENDCOLOR}:  ${F}"  1>&2
     done
-    if [[ ${ALL_OK} -eq 0 ]] ; then echo -e "${RED}File(s) NOT FOUND; Aborting${ENDCOLOR}; Check your inputs" ; exit 1 ; fi
+    if [[ ${ALL_OK} -eq 0 ]] ; then echo -e "${RED}File(s) NOT FOUND; Aborting${ENDCOLOR}; Check your inputs"  1>&2 ; exit 1 ; fi
 }
 
 function usage(){
@@ -59,11 +59,11 @@ bcftools view -H | \
 awk '{FS=OFS="\t" ; $9="GT:"$9 ; $10="./.:"$10 ; print }' ) | \
 bcftools +fill-tags - -- -t 'FORMAT/DP=int(smpl_sum(PR+SR)),FORMAT/AR=float((PR + SR)/smpl_sum(PR+SR)),FORMAT/AD=int(PR+SR)' | \
 bcftools +setGT -O v -o "${VCF_PREPPED_OUTFILENAME}"  - -- -t a -n m
-check_ev $? "bcftools SV prep"
+check_ev $? "bcftools SV prep"  1>&2
 
 ## indexing newly created vcf.gz file
 bcftools index --force "${VCF_PREPPED_OUTFILENAME}"
-check_ev $? "bcftools SV prep index"
+check_ev $? "bcftools SV prep index"  1>&2
 
 
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -88,7 +88,7 @@ else
     echo -e "for consistency, we are creating the expected out file here by copying as is the file << ${VCF_PREPPED_OUTFILENAME} >> to << ${VCF_PREPPED_OUTFILENAME_DEDUP} >> "  1>&2
 
     cp "${VCF_PREPPED_OUTFILENAME}" "${VCF_PREPPED_OUTFILENAME_DEDUP}"
-    check_ev $? "copy VCF"
+    check_ev $? "copy VCF"  1>&2
 
 fi
 
